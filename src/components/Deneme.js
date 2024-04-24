a9b8c5bc6a17792d0ef228f942a8feddc3e5f658
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2024-04-24 17:42:02 +0300

    Proje oluşturuldu.

---
+++ src/components/Deneme.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Deneme = () => {
+  return (
+    <View>
+      <Text>Deneme</Text>
+    </View>
+  )
+}
+
+export default Deneme
+
+const styles = StyleSheet.create({})
