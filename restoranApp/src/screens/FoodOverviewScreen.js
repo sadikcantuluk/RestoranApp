2dfec3530897e7575bec4ae84450142dd7cb4587
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2024-04-27 18:27:25 +0300

    Menü isimlerinin dinamik olarak sayfa başlığına basılması.

--- restoranApp/src/screens/FoodOverviewScreen.js
+++ restoranApp/src/screens/FoodOverviewScreen.js
@@ -1,34 +1,46 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { FOODS } from '../data/dummy-data';
-import FoodItem from '../components/FoodItem';
+import { FlatList, StyleSheet, Text, View} from "react-native";
+import React, { useLayoutEffect } from "react";
+import { FOODS, CATEGORIES } from "../data/dummy-data";
+import FoodItem from "../components/FoodItem";
 
-export default function FoodOverviewScreen({route}) {
+export default function FoodOverviewScreen({ route, navigation }) {
+    
+  const categoryId = route.params.categoryId;
 
-    const categoryId = route.params.categoryId;
+  const displayedFoods = FOODS.filter((foodItem) => {
+    return foodItem.categoryIds.indexOf(categoryId) >= 0;
+  });
 
-    const displayedFoods = FOODS.filter((foodItem)=>{
-        return foodItem.categoryIds.indexOf(categoryId) >= 0;
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === categoryId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
     });
+  }, [categoryId, navigation]);
 
-    const renderFoodItem = (itemData) => {
-        const foodItemsProps = {
-            id: itemData.item.id,
-            title: itemData.item.title,
-            imageUrl: itemData.item.imageUrl,
-            affordability: itemData.item.affordability,
-            complexity: itemData.item.complexity,
-        }
-        return(
-            <FoodItem {...foodItemsProps}/>
-        )
-    }
+  const renderFoodItem = (itemData) => {
+    const foodItemsProps = {
+      id: itemData.item.id,
+      title: itemData.item.title,
+      imageUrl: itemData.item.imageUrl,
+      affordability: itemData.item.affordability,
+      complexity: itemData.item.complexity,
+    };
+    return <FoodItem {...foodItemsProps} />;
+  };
 
   return (
     <View>
-      <FlatList data={displayedFoods} keyExtractor={(item)=>item.id} renderItem={renderFoodItem}/>
+      <FlatList
+        data={displayedFoods}
+        keyExtractor={(item) => item.id}
+        renderItem={renderFoodItem}
+      />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
