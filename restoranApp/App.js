a26e582b45ce77c02ff46a0b9463b19084c845b2
Sadikcan Tuluk <sadikcantuluk@gmail.com>
2024-04-28 14:48:38 +0300

    Drawer Navigater yapısı projeye eklendi.

--- restoranApp/App.js
+++ restoranApp/App.js
@@ -5,31 +5,66 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import FoodOverviewScreen from "./src/screens/FoodOverviewScreen";
 import FoodDetailScreen from "./src/screens/FoodDetailScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import FavoritesScreen from "./src/screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#FFC7C7",
+        },
+        contentStyle: {
+          backgroundColor: "#FFE2E2",
+        },
+        headerTintColor:'#3C5B6F'
+      }}
+    >
+      <Drawer.Screen
+        name="Category"
+        component={CategoryScreen}
+        options={{
+          title: "Kategoriler Sayfası",
+        }}
+      />
+      <Drawer.Screen name="Favorite" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerStyle:{
-          backgroundColor:'#FFC7C7'
-        },
-        contentStyle:{
-          backgroundColor:'#FFE2E2'
-        }
-      }}>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#FFC7C7",
+          },
+          contentStyle: {
+            backgroundColor: "#FFE2E2",
+          },
+          headerTintColor:'#3C5B6F'
+        }}
+      >
         <Stack.Screen
-          name="Category"
-          component={CategoryScreen}
+          name="Drawer"
+          component={DrawerNavigator}
           options={{
-            title: "Kategoriler Sayfası",
+            headerShown: false,
           }}
         />
-        <Stack.Screen name="FoodOverview" component={FoodOverviewScreen}/>
-        <Stack.Screen name="FoodDetail" component={FoodDetailScreen} options={{
-          title:'Yemek İçeriği'
-        }}/>
+        <Stack.Screen name="FoodOverview" component={FoodOverviewScreen} />
+        <Stack.Screen
+          name="FoodDetail"
+          component={FoodDetailScreen}
+          options={{
+            title: "Yemek İçeriği",
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
